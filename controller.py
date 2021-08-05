49d8d6294537ab638f1a93de5427c419ac0958df
iicfcii <8230152+iicfcii@users.noreply.github.com>
2021-08-04 22:04:34 -0700

    Added blind multi jump

--- controller.py
+++ controller.py
@@ -113,3 +113,68 @@ class MultiJump():
                 self.state = 'extending'
 
         self.f_contact_pre = f_contact
+
+class BlindMultiJump():
+    t_settle = 1.0
+
+    def __init__(self, model):
+        self.model = model
+
+        self.q1_i, self.q2_i = self.model.leg.est_ik(-PI/2,self.model.leg.lmax)
+
+        self.lr = 0.04
+        self.le = 0.08
+        self.lt = -PI/2
+        self.p = 0.2
+
+        self.state = 'falling'
+        self.t_pre = None
+
+        self.hip_servo = motor.Servo(
+            lambda:self.model.motor_hip.GetMotorRot(),
+            pdi=[1,150,0],
+            name='hip'
+        )
+        self.knee_servo = motor.Servo(
+            lambda:self.model.motor_crank1.GetMotorRot(),
+            pdi=[1,150,0],
+            name='knee'
+        )
+        self.model.motor_crank1.SetTorqueFunction(self.knee_servo)
+        self.model.motor_hip.SetTorqueFunction(self.hip_servo)
+
+    def control(self):
+        t = self.model.system.GetChTime()
+
+        if self.state == 'falling':
+            self.model.body.SetBodyFixed(False)
+            q1, q2 = self.model.leg.est_ik(self.lt,self.lr)
+            self.hip_servo.set_t(q1-self.q1_i)
+            self.knee_servo.set_t(q2-self.q2_i)
+
+            if t > Jump.t_settle:
+                self.state = 'extending'
+
+        if self.state == 'extending':
+            q1, q2 = self.model.leg.est_ik(self.lt,self.le)
+            self.hip_servo.set_t(q1-self.q1_i)
+            self.knee_servo.set_t(q2-self.q2_i)
+
+            if self.t_pre is None:
+                self.t_pre = t
+            else:
+                if t - self.t_pre > self.p/2:
+                    self.state = 'retracting'
+                    self.t_pre = None
+
+        if self.state == 'retracting':
+            q1, q2 = self.model.leg.est_ik(self.lt,self.lr)
+            self.hip_servo.set_t(q1-self.q1_i)
+            self.knee_servo.set_t(q2-self.q2_i)
+
+            if self.t_pre is None:
+                self.t_pre = t
+            else:
+                if t - self.t_pre > self.p/2:
+                    self.state = 'extending'
+                    self.t_pre = None
